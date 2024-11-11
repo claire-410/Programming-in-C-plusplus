e24f08b0d28b4dc205f90821738a8f1b65d34aab
Claire <105070638+claire-410@users.noreply.github.com>
2024-11-11 02:30:25 -0500

    Update Workstation.cpp

--- projects/ms3/Workstation.cpp
+++ projects/ms3/Workstation.cpp
@@ -1,9 +1,4 @@
-// Name:  Reziyemu Sulaiman
-// Seneca Student ID: 106-153-208
-// Seneca email: rsulaiman2@myseneca.ca
-// Date of completion: 2022-12-03
-//
-// I have done all the coding by myself and only copied the code that my classmate provided to complete my workshops and assignments.
+
 
 
 #include "Workstation.h"
