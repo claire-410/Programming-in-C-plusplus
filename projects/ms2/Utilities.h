d816561696e34387ef4abcd1c7fabf5bfddae304
Claire <105070638+claire-410@users.noreply.github.com>
2024-11-11 02:25:46 -0500

    Update Utilities.h

--- projects/ms2/Utilities.h
+++ projects/ms2/Utilities.h
@@ -1,9 +1,4 @@
-// Name:  Reziyemu Sulaiman
-// Seneca Student ID: 106-153-208
-// Seneca email: rsulaiman2@myseneca.ca
-// Date of completion: 2022-11-25
-//
-// I have done all the coding by myself and only copied the code that my classmate provided to complete my workshops and assignments.
+
 
 
 #ifndef SDDS_UTILITIES_H
